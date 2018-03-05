50e824f524091400b5e2f0f35438a4e412b92df7
RagingComputer <raging@ragingcomputer.com>
2018-03-04 20:40:38 -0600

    Initial Commit - works on my raspberry pi with raspbian

---
+++ settings_template.py
@@ -0,0 +1,17 @@
+# List of the Meter IDs to watch
+# Use empty brackets to read all meters - []
+# List may contain only one entry - [12345678]
+# or multiple entries - [12345678, 98765432, 12340123]
+WATCHED_METERS = []
+
+# MQTT Server settings
+# MQTT_HOST needs to be a string
+# MQTT_PORT needs to be an int
+MQTT_HOST = '127.0.0.1'
+MQTT_PORT = 1883
+
+# path to rtlamr
+RTLAMR = '/usr/local/bin/rtlamr'
+
+# path to rtl_tcp
+RTL_TCP = '/usr/bin/rtl_tcp'
