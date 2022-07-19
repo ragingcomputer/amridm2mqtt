aa9a5fc5504b3d5f67d38bf168ac407ecf4ddef1
Justin Phelps <juphelps@netflix.com>
2022-07-19 14:43:14 -0600

    add our DEBUG and MESSAGE_TYPE options to settings.

--- settings_template.py
+++ settings_template.py
@@ -40,3 +40,15 @@ RTLAMR = '/usr/local/bin/rtlamr'
 
 # path to rtl_tcp
 RTL_TCP = '/usr/bin/rtl_tcp'
+
+# MESSAGE_TYPE we are looking for
+# scm: Standard Consumption Message. Simple packet that reports total consumption.
+# scm+: Similar to SCM, allows greater precision and longer meter ID's.
+# idm: Interval Data Message. Provides differential consumption data for previous 47 intervals at 5 minutes per interval.
+# netidm: Similar to IDM, except net meters (type 8) have different internal packet structure, number of intervals and precision. Also reports total power production.
+# r900: Message type used by Neptune R900 transmitters, provides total consumption and leak flags.
+# r900bcd: Some Neptune R900 meters report consumption as a binary-coded digits.
+MESSAGE_TYPE = 'idm'
+
+# DEBUG to output debug information
+DEBUG = False
